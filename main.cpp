758670b8a3084c09e7345f2178e6251350c3f674
MostafaOmar98 <30315707+MostafaOmar98@users.noreply.github.com>
2018-11-16 23:01:56 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
+/*
+ * Name: Vending Machine Simulator
+ * Purpose: Simulate user experience using Vending Machine
+ * Author 1: Mostafa Omar Mahmoud 20170292 G16
+ * Author 2: Ahmed Mohamed Hanafy 20170357 G15
+ * Date: Nov. 16th 2018
+ * Version: 1.1
+ */
+
+
 #include <iostream>
 using namespace std;
 #include "Money.h"
@@ -6,7 +16,6 @@ using namespace std;
 
 int main()
 {
-//    freopen("input.txt", "r", stdin);
     VendingMachine V;
     V.setVendingMachine();
     V.runVendingMachine();
